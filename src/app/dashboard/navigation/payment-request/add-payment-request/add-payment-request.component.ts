a68899cc5f989b93a98e8ea76a516187c9a837bc
shrinivas <shrinivas@foreignadmits.com>
2022-03-26 10:23:45 +0530

    payment request List View

---
+++ src/app/dashboard/navigation/payment-request/add-payment-request/add-payment-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-payment-request',
+  templateUrl: './add-payment-request.component.html',
+  styleUrls: ['./add-payment-request.component.css']
+})
+export class AddPaymentRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
