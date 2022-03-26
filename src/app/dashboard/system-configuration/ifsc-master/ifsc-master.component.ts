b15abc5030b8baf9f0f42c0ce0a172ca1be989da
shrinivas <shrinivas@foreignadmits.com>
2022-03-26 17:11:36 +0530

    list View

--- src/app/dashboard/system-configuration/ifsc-master/ifsc-master.component.ts
+++ src/app/dashboard/system-configuration/ifsc-master/ifsc-master.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ifsc-master',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ifsc-master.component.css']
 })
 export class IfscMasterComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private router: Router) { }
 
   ngOnInit(): void {
+    // this.bank = this.data.getBank()
+    // this.data.bankChange.subscribe((bank: Bank[]) => {
+    //   this.bank = bank
+    // })   
+  }
+  // addNewbank() {
+  //   this.router.navigate(['/dashboard/system-configuration/bank/add'])
+  // }
+  addIfsc() {
+    this.router.navigate(['/dashboard/system-configuration/ifscMaster/add'])
   }
 
 }
--- src/app/dashboard/system-configuration/vendor/vendor.component.ts
+++ src/app/dashboard/system-configuration/vendor/vendor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Vendor } from 'src/app/shared/data.model';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-vendor',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor.component.css']
 })
 export class VendorComponent implements OnInit {
-
-  constructor() { }
+  vendor: any
+  constructor(private data: DataService,
+    private router: Router) { }
 
   ngOnInit(): void {
-  }
+    this.vendor = this.data.getVendor()
+    console.log(this.vendor);
+    this.data.vendorChange.subscribe((vendor: Vendor[]) => {
+      this.vendor = vendor
+      console.log(vendor);
 
+    })
+  }
+  addvendor() {
+    this.router.navigate(['/dashboard/system-configuration/vendor/add'])
+  }
 }
+
+
