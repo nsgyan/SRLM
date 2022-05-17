15c14a506857866a1d0646d0098f28a1abcc9d05
Shri nivas <shri.nivas@troology.com>
2022-05-17 10:06:12 +0530

    git push

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ciifaceproject'),
+      dir: require('path').join(__dirname, './coverage/upSrlm'),
       subdir: '.',
       reporters: [
         { type: 'html' },
