131e5760e0330a130136daebc8d466953fb55a1b
Shri nivas <shri.nivas@troology.com>
2022-06-30 15:10:39 +0530

    git push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production  : true,
-  // serverUrl:'http://upsrlm.evalue8.info:3000/'
-  serverUrl: 'http://localhost:3000/'
+  serverUrl:'http://upsrlm.evalue8.info/'
+  // serverUrl: 'http://localhost:3000/'
 };
